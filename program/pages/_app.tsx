ee903b9cdf57b25e683e3c068a4ffff78bc893dc
0918nobita <nobita.0918@gmail.com>
2021-01-06 15:25:04 +0900

    :construction: Service Worker を追加

--- program/pages/_app.tsx
+++ program/pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
+    useEffect(() => {
+        if (!('serviceWorker' in navigator)) return;
+        navigator.serviceWorker.register('/sw.js').then((regisration) => {
+            console.log('Service Worker regisration successful with scope: ', regisration.scope);
+        });
+    }, []);
+
     return <Component {...pageProps} />
 };
 
---
+++ program/public/sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', (event) => {
+    self.console.log("Hello world from the Service Worker");
+});
